a8936a4ff2af4d9ceeddd26bd9b9b04bb446f24c
shalini <s.rathore43@gmail.com>
2025-05-05 11:02:07 +0530

    ijo

--- Controller/userController.js
+++ Controller/userController.js
@@ -9,3 +9,21 @@ exports.getUserById=(id)=>
     let u = users.find((usr)=>usr.id==id)
     return u 
 }
+exports.adduser = (u)=>
+{
+    users.push(u)
+    return "user added"
+}
+exports.updateUser=(user123)=>
+{
+    let id = parseInt(user123.id)
+    let index = users.findIndex((u)=> u.id==id)
+ 
+    if(index>=0)
+    {
+        users[index].name = user123.name
+        users[index].contactno = user123.contact
+        return "record updated"
+    }
+    return "record not found";
+}
